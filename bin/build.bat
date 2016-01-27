e297fd7a841f650786889e8611570de7e8f32b92
Maxwell Slater <mslater@nevada.unr.edu>
2016-01-27 14:03:17 -0800

    Init 4

--- bin/build.bat
+++ bin/build.bat
@@ -7,7 +7,7 @@ pushd w:\build
 :: setup flags for cl.exe
 set CompileFlags=/I"W:\deps\include" /Od /Zi /w /MD /EHsc 
 set LinkFlags=/incremental:NO /NODEFAULTLIB:msvcrtd.lib /subsystem:console,5.01 W:\deps\lib\SDL2.lib W:\deps\lib\SDL2main.lib  W:\deps\lib\cyanlogger2.lib W:\deps\lib\SDL2_image.lib W:\deps\lib\SDL2_mixer.lib
-set Files=w:\code\main.cpp
+set Files=w:\code\engine\main.cpp
 
 :: actually call cl
 cl /Fe:Game.exe %CompileFlags% %Files% /link %LinkFlags%
