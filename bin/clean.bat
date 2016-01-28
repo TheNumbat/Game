92cb89dc1a11848d211cce8dde3f8acc305c4ee4
TheMaxS <mslater@nevada.unr.edu>
2016-01-27 21:01:16 -0800

    Clean

--- bin/clean.bat
+++ bin/clean.bat
@@ -2,10 +2,17 @@
 
 pushd "W:/build"
 
-del *.exe > NUL 2> NUL
-del *.obj > NUL 2> NUL
-del *.pdb > NUL 2> NUL
 del *.ilk > NUL 2> NUL
+del *.obj > NUL 2> NUL
+del *.exe > NUL 2> NUL
+del *.pdb > NUL 2> NUL
+del *.sdf > NUL 2> NUL
+del *.opensdf > NUL 2> NUL
+del *.txt > NUL 2> NUL
+del *.exp > NUL 2> NUL
+del *.lib > NUL 2> NUL
 del *.map > NUL 2> NUL
+del Game.dll > NUL 2> NUL
+del GameTemp.dll > NUL 2> NUL
 
 popd
