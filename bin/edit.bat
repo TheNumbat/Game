85dcdac63fad25eccf53fc37e7b87fdf5a9cbe80
TheMaxS <mslater@nevada.unr.edu>
2016-01-29 21:33:52 -0800

    edit

--- bin/edit.bat
+++ bin/edit.bat
@@ -1,3 +1,4 @@
 @echo off
 
-"C:\Program Files\Sublime Text 3\subl.exe" -a W:\code\
+IF EXIST "C:\Program Files\Sublime Text 3" "C:\Program Files\Sublime Text 3\subl.exe" -a W:\code\
+IF EXIST "C:\Program Files\Sublime Text 2" "C:\Program Files\Sublime Text 2\subl.exe" -a W:\code\
