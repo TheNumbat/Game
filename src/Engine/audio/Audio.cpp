92523dd38a1b8bbce6799c9a8e603282a0dbcfb6
Maxwell Slater <mslater@nevada.unr.edu>
2016-08-26 19:20:45 -0700

    returns (for testing)

--- src/Engine/audio/Audio.cpp
+++ src/Engine/audio/Audio.cpp
@@ -43,51 +43,51 @@ Audio::Audio() {
 Audio::~Audio() {
 
 }
-
+ 
 bool Audio::init() {
-
+	return true;
 }
 
 bool Audio::kill() {
-
+	return true;
 }
 
 bool Audio::loadSound(const std::string& sID, const std::string& path) {
-
+	return true;
 }
 
 bool Audio::laodSoundFolder(const std::string& path) {
-
+	return true;
 }
 
 bool Audio::freeSound(const std::string& sID) {
-
+	return true;
 }
 
 bool Audio::play(const std::string& sID, u32 loop, s32 timeout) {
-
+	return true;
 }
 
 bool Audio::pause(const std::string& sID) {
-
+	return true;
 }
 
 bool Audio::resume(const std::string& sID) {
-
+	return true;
 }
 
 bool Audio::stop(const std::string& sID, u32 timeout) {
-
+	return true;
 }
 
 bool Audio::pauseAll() {
-
+	return true;
 }
 
 bool Audio::resumeAll() {
-
+	return true;
 }
 
 bool Audio::stopAll(u32 timeout) {
-
+	return true;
 }
